0d9bbce15d6840f2e202f2f2a8f670dc967e2101
C Jay Marshall <cjaymarshall1@hotmail.com>
2018-08-11 09:39:18 -0600

    still trying to fix things

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,12 +12,5 @@ class ApplicationController < ActionController::Base
   end
 end
 
-module CjmViewTool
-  class Renderer
-    def self.copyright name, msg
-      "&copy; 2006-#{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
-    end
-  end
-end
 
 
